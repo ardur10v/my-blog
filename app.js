e60bdb38ceaed026bf1997ebef167ffba9800e48
ardur10v <1032221511@tcetmumbai.in>
2025-06-09 23:36:32 +0530

    env issue

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app=express()
 
 const PORT=process.env.PORT || 8000
 
-mongoose.connect(process.env.MONGO_URL,{
+mongoose.connect(process.env.MONGO_URI,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
